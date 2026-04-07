3c26f9b9c9fa9344d56093b98b0f62ff2d05e9ae
sushant <118897403+sushant-demn@users.noreply.github.com>
2026-04-07 19:59:31 +0530

    Visuals (#4)
    * Added Buttons on Main Menu
    
    * Main Menu Polish and Audio
    
    ---------
    
    Co-authored-by: Tanish Patil <drac29673@gmail.com>

---
+++ Assets/Scripts/UIButtonEffects.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIButtonEffects : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+{
+    private Vector3 originalScale;
+
+    public float hoverScale = 1.1f;
+    public float clickScale = 0.95f;
+    public PacmanSelector selector;
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = originalScale * hoverScale;
+
+        selector.MoveTo(transform);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = originalScale;
+
+        selector.ClearTarget(transform);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.localScale = originalScale * clickScale;
+    }
+
+}
